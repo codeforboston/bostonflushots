374514bfa6822570cfef578eb847a43baa73487e
Michael Lawrence Evans <michael@codeforamerica.org>
2013-01-11 22:07:45 -0500

    Fixing

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@ var config = {
     "table_id": "1lCrp_Ee2AcV-fieOYf8Q2sHcDLesikoe42r_M6A",
     "default_lat": "42.33",
     "default_lng": "-71.05977",
-    "initial_zoom": "11",
+    "initial_zoom": 11,
     "map_style": "grey"
 }
