3539a1166e7734caf9702caa80948983bbccc649
Michael Lawrence Evans <michael@codeforamerica.org>
2013-01-11 22:03:00 -0500

    ch-ch-ch-changes

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 var config = {
     "api_key": "AIzaSyBtTuNj9M-0cC6sPsqsBcnnQ-wTlB1tSsc",
+    "table_id": "1lCrp_Ee2AcV-fieOYf8Q2sHcDLesikoe42r_M6A",
     "default_lat": "42.33",
     "default_lng": "-71.05977"
 }
